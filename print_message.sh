c0eb3344cb888014bf8fafe2c2cef011d9f7e16f
Elena <89459474+oivie@users.noreply.github.com>
2023-08-15 15:07:25 -0400

    Create print_message.sh

---
+++ print_message.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Description: Printing custom message script
+# Author: Elena Pashkova
+
+print_custom_message()
+{
+
+MESSAGE="Helllo World!"
+echo "============================"
+echo "$MESSAGE"
+echo "============================"
+}
+
+
+print_custom_message $1
